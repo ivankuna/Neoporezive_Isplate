22d1d7edc01d6b40a4d4ae88b15c568fb7974747
ivankuna <ivan.kuna@yahoo.com>
2023-05-03 11:46:34 +0200

    Neoporezive isplate 03.05.2023. 001

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -13,7 +13,7 @@ import static Servis.DatabaseUtils.getRowCount;
 import static Servis.DatabaseUtils.readFromTable;
 
 public class test {
-    public static void main(String[] args) throws JAXBException, SQLException {
+    public static void createXMLOsobe() throws JAXBException, SQLException {
         List<Osobe> osobeList = new ArrayList<>();
         int rowCount = getRowCount("osobe");
         File file = new File("xml/osobe.xml");
@@ -46,4 +46,47 @@ public class test {
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
         marshaller.marshal(popisOsoba, new File("xml/osobe.xml"));
     }
+
+    public static void createXML() throws JAXBException, SQLException {
+        List<Osobe> osobeList = new ArrayList<>();
+        int rowCount = getRowCount("osobe");
+        File file = new File("xml/osobe.xml");
+
+        if(file.exists()) {
+            file.delete();
+            System.out.println("File deleted.");
+        } else {
+            // Do nothing
+        }
+
+        for (int i = 0; i < rowCount; i++) {
+            String[] osobe = readFromTable("osobe", "id_osobe", i + 1);
+            Osobe osoba = new Osobe();
+            assert osobe != null;
+            osoba.setId_osobe(Integer.parseInt(osobe[0]));
+            osoba.setIme(osobe[1]);
+            osoba.setPrezime(osobe[2]);
+            osoba.setOib(osobe[3]);
+            osoba.setIban(osobe[4]);
+            osobeList.add(osoba);
+        }
+        // Create a wrapper class that contains the list of Osobe instances
+        PopisOsoba popisOsoba = new PopisOsoba();
+        popisOsoba.setOsoba(osobeList);
+
+        // Marshal the wrapper class to XML
+        JAXBContext jaxbContext = JAXBContext.newInstance(PopisOsoba.class);
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.marshal(popisOsoba, new File("xml/osobe.xml"));
+    }
+
+    public static void main(String[] args) {
+        try {
+            createXML();
+        } catch (JAXBException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
